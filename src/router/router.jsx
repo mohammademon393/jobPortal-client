a48851cd186c45dffcc39943310f184a7038a7cf
mohammademon393 <mdemon357ep@gmail.com>
2026-06-23 21:22:22 +0600

    Setup tailwind and daisyUi

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter } from "react-router";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World, setup router</div>,
+    element: <div className="text-red-500">Hello World, setup router</div>,
   },
 ]);
 
