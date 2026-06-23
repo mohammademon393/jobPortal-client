86a5e41da4f29ae713b01ebbb08f3571a681b1b6
mohammademon393 <mdemon357ep@gmail.com>
2026-06-23 21:06:26 +0600

    Installed react router and setUp route render

---
+++ src/router/router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World, setup router</div>,
+  },
+]);
+
+export default router;
